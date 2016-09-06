d53e41ac47576cbf63d41fdbbd3ef1f963793c43
SalvatierraFran <franco14_93@hotmail.com>
2016-09-06 18:42:34 -0300

    Primeros ejercicios de PHP+HTML

---
+++ Aplicacion20/Aplicacion20.php
@@ -0,0 +1,15 @@
+<?php
+$variable="red";
+$variable2="blue";
+?>
+
+<!DOCTYPE html>
+<html lang ="es">
+<head><meta charset="utf-8"/>
+</head>
+<!-- bgcolor=<?php echo $variable;?> --> 
+<body style=background-color:<?php echo $variable2; ?>>
+
+</body>
+
+</html>
---
+++ Aplicacion21/Aplicacion21.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head><meta charset="utf-8"/>
+</head>
+<body>
+
+	<table>
+		<tr>
+			<td>Hola
+			<td>Hola
+		</tr>
+		<tr>
+			<td>Chau
+			<td>Chau
+		</tr>
+	</table>
+
+</body>
+</html>
